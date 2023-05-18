7a04a0387eaa371d648372b6b0b9c9cc3f6c1b69
madhuri yadav <mayuriyadav54@gmail.com>
2023-05-18 17:29:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 console.log("Hello World");
-console.log("Mayuri");
-console.log("M");
+import express from "express";
+import { kunal, Swaraj, Anu, Krishnan, Abhi } from './controllers/All-Controllers.js';
+
+const app = express();
+
+app.get('/kunal', kunal);
+app.get('/anu', Anu);
+app.get('/abhi', Abhi);
+app.get('/kirshna', Krishnan);
+app.get('/swaraj', Swaraj); // pass two parameters, first is path, second is function
+// app.post();
+// app.patch();
+// app.put();
+// app.delete();
+
+
+
+app.listen(8000); // port
