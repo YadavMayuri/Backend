56585354f7d816bc0b2a2a5577c25690d1e0d79f
madhuri yadav <mayuriyadav54@gmail.com>
2023-05-24 12:07:21 +0530

    updates

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 
 import { mayuri, abc, Anu, Krishnan, megha } from './controllers/All-Controllers.js';
 import router from "./routes/UserRoutes.js";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -12,6 +13,10 @@ const app = express();
 app.use(morgan('dev')); // use() - middleware
 app.use(express.json()); // data to parse
 app.use('/api/v1', router);
+
+mongoose.connect('mongodb+srv://mayuriyadav54:HGU1ZbJCNcqlTu0z@cluster0.s9gcceb.mongodb.net/awdizDB')
+.then(()=> console.log("DB Connected"))
+.catch((err) => console.log("DB Error =>", err));
 
 // app.get('/mayuri', mayuri);
 // app.get('/anu', Anu);
