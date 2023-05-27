5d81027fd48b5b87c017c721f3f84c47f1a1fb0f
madhuri yadav <mayuriyadav54@gmail.com>
2023-05-27 13:27:26 +0530

    updated

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,8 +6,8 @@ import { abc, mayuri } from "../controllers/All-Controllers.js";
 
 var router = express.Router();
 
-router.get('/login', login);
-router.get('/register', register);
+router.post('/login', login);
+router.post('/register', register);
 router.post('/add-product', addProduct);
 router.get('/mayuri',mayuri)
 router.get('/abc',abc)
